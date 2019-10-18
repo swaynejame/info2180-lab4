58c988562799a3da5deca69745de104bf0c2563c
swaynejame <swaynejame11@gmail.com>
2019-10-18 13:07:30 -0500

    exercise 2,4 & 6 complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,13 +1,59 @@
-//EVENT ONE
-//const showBoard document.querySelector('#board');
-//var boardSet = document.getElementById("#board").appendChild(); 
-//var parent = getElementById("#board");
 function tictactoe(){
-	//document.querySelector("#board").classList.add("square");
-	var boardBoxes = document.getElementById("board").querySelectorAll("div");
-	for(x=0; x<boardBoxes.length; x++){
-		boardBoxes[x].classList.add("square");
+
+	var board = document.querySelector("#board");
+	var boardBoxes = board.querySelectorAll("div");
+	var EMPTY ='';
+	var tracker=0;
+	var cellNum=0;
+	var x_O;
+	var stat = document.querySelector("#status");
+
+	boardBoxes.forEach(element =>{
+		element.setAttribute("class","square");//Adds class with value square to each div
+		element.textContent =EMPTY;
+		element.setAttribute("id",cellNum++);
+	});
+
+	boardBoxes.forEach(element => {
+		element.addEventListener("click",function(){
+			if (element.textContent !== EMPTY){
+				return; //alert("Stop trying to cheat");
+			}
+			tracker+=1;
+			if(tracker %2 ==0){
+				this.textContent='O';
+				this.classList.add('square','O');
+			}
+			else{
+				this.textContent='X';
+				this.classList.add('square','X');
+			}
+			gameOver();
+		});//end inner
+	});//end outer
+
+	function gameOver(){
+		if(boardBoxes[0].textContent==boardBoxes[1].textContent && boardBoxes[1].textContent==boardBoxes[2].textContent &&boardBoxes[0].textContent==boardBoxes[2].textContent && boardBoxes[2].textContent !== "")
+			results(boardBoxes[0].textContent);
+		if(boardBoxes[3].textContent==boardBoxes[4].textContent && boardBoxes[4].textContent==boardBoxes[5].textContent &&boardBoxes[3].textContent==boardBoxes[5].textContent && boardBoxes[5].textContent !== "")
+			results(boardBoxes[3].textContent);
+		if(boardBoxes[6].textContent==boardBoxes[7].textContent && boardBoxes[7].textContent==boardBoxes[8].textContent &&boardBoxes[6].textContent==boardBoxes[8].textContent && boardBoxes[8].textContent !== "")
+			results(boardBoxes[6].textContent);
+		if(boardBoxes[0].textContent==boardBoxes[3].textContent && boardBoxes[3].textContent==boardBoxes[6].textContent &&boardBoxes[0].textContent==boardBoxes[6].textContent && boardBoxes[6].textContent !== "")
+			results(boardBoxes[0].textContent);
+		if(boardBoxes[1].textContent==boardBoxes[4].textContent && boardBoxes[4].textContent==boardBoxes[7].textContent &&boardBoxes[1].textContent==boardBoxes[7].textContent && boardBoxes[7].textContent !== "")
+			results(boardBoxes[1].textContent);
+		if(boardBoxes[2].textContent==boardBoxes[5].textContent && boardBoxes[5].textContent==boardBoxes[8].textContent &&boardBoxes[2].textContent==boardBoxes[8].textContent && boardBoxes[8].textContent !== "")
+			results(boardBoxes[2].textContent);
+		if(boardBoxes[0].textContent==boardBoxes[4].textContent && boardBoxes[4].textContent==boardBoxes[8].textContent &&boardBoxes[0].textContent==boardBoxes[8].textContent && boardBoxes[8].textContent !== "")
+			results(boardBoxes[0].textContent);
+		if(boardBoxes[6].textContent==boardBoxes[4].textContent && boardBoxes[4].textContent==boardBoxes[2].textContent &&boardBoxes[6].textContent==boardBoxes[2].textContent && boardBoxes[2].textContent !== "")
+			results(boardBoxes[6].textContent);
 	}
 
+	var results = function(x_O){
+		stat.classList.add("class", "you-won");
+    	stat.textContent = `Congratulations! ${x_O} is the Winner!`;
+	}
 }
 window.onload=tictactoe;
